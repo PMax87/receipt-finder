2b3e3830e4698cd75e191d9f73efaecbc79128b3
PMax87 <noitulove@gmail.com>
2023-10-07 09:34:15 +0200

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="w-[100%] shadow-lg h-[70px]">
+      <div className="container mx-auto h-full"></div>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
